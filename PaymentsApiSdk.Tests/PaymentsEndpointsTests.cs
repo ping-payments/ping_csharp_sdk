2f8657c1bea98140c5a61f3053693a59bf4114f7
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2022-03-17 19:04:38 +0100

    Adding support for 404

--- PaymentsApiSdk.Tests/PaymentsEndpointsTests.cs
+++ PaymentsApiSdk.Tests/PaymentsEndpointsTests.cs
@@ -92,5 +92,46 @@ namespace PaymentsApiSdk.Tests
             Assert.NotNull(body.Errors[0].Error);
             Assert.NotNull(body.Errors[0].Property);
         }
+
+
+        [Fact]
+        public async Task Can_handle_404_when_a_payment_order_does_not_exist()
+        {
+            var tenantId = Guid.Parse("be74903f-72bd-4e21-97c4-128dcf85e2f0");
+            var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://sandbox.pingpayments.com/payments/")
+            };
+            var api = new PaymentsApiClient(tenantId, httpClient);
+            var orderId = Guid.NewGuid();
+            var requestObject = new InitiatePaymentRequest
+            (
+                CurrencyEnum.SEK,
+                new Dictionary<Guid, int> { { Guid.Parse("04476abd-4bd4-45bb-b6ea-dcda41aded4d"), 1000 } },
+                new Dictionary<string, object> { { "test_data", 1337m } },
+                new OrderItem[]
+                {
+                    new OrderItem(500, "A", 0.25m),
+                    new OrderItem(500, "B", 0.12m),
+                },
+                MethodEnum.dummy,
+                ProviderEnum.dummy,
+                new DummyProviderMethodParameters(),
+                new Uri("https://not.real.callback.pingpayments.com")
+            );
+            var response = await api.Payments.InitiatePayment(orderId, requestObject);
+            Assert.NotNull(response);
+            Assert.Equal(404, response.StatusCode);
+            Assert.True(response.IsFailure);
+            Assert.False(response.IsSuccessful);
+            Assert.NotNull(response.Body);
+            Assert.NotNull(response.Body.ErrorResponseBody);
+            ErrorResponseBody body = response.Body.ErrorResponseBody;
+            Assert.NotNull(body);
+            Assert.True(body.Errors.Any());
+            Assert.NotNull(body.Errors[0].Description);
+            Assert.NotNull(body.Errors[0].Error);
+            Assert.Null(body.Errors[0].Property);
+        }
     }
 }
