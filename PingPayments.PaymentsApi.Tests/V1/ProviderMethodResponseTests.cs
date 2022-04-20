16cf7a61613bdf0525e9505ff78e50bff56a0ce6
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2022-04-20 18:23:22 +0200

    Fixing tests

--- PingPayments.PaymentsApi.Tests/V1/ProviderMethodResponseTests.cs
+++ PingPayments.PaymentsApi.Tests/V1/ProviderMethodResponseTests.cs
@@ -46,7 +46,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.m_commerce,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"url\":\"swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=merchant%253A%252F%252F\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() }}
-                ) is SwishMCommerceResponse
+                ) is SwishMCommerceResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.SwishUrl)
             );
 
         [Fact]
@@ -59,7 +60,9 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.m_commerce,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"url\":\"swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=merchant%253A%252F%252F\",\"qr_code\":\"<svg>foo</svg>\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() } }
-                ) is SwishMCommerceResponse
+                ) is SwishMCommerceResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.SwishUrl) &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.QrCode)
             );
 
         [Fact]
@@ -72,7 +75,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.card,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"redirect_url\":\"https://verifone.com/pay/whatever\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() }}
-                ) is VerifoneResponse
+                ) is VerifoneResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.RedirectUrl)
             );
         
         [Fact]
@@ -85,7 +89,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.invoice,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"invoice_url\":\"https://billmate.com/invoice\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() }}
-                ) is BillmateResponse
+                ) is BillmateResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.InvoiceUrl)
             );
         
         [Fact]
@@ -98,7 +103,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.card,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"pay_url\":\"https://payment-iq.com/pay\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() }}
-                ) is PaymentIqResponse
+                ) is PaymentIqResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.PayUrl)
             );
 
         [Fact]
@@ -111,7 +117,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.vipps,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"pay_url\":\"https://payment-iq.com/pay\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() }}
-                ) is PaymentIqResponse
+                ) is PaymentIqResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.PayUrl)
             );
 
         [Fact]
@@ -124,7 +131,8 @@ namespace PingPayments.PaymentsApi.Tests.V1
                     MethodEnum.deposit,
                     "{\"id\":\"15c44587-7ebb-43a3-b437-8d00e5f8df7a\",\"provider_method_response\":{\"reference\":\"abcd1234\"}}",
                     new() { Converters = { new MethodEnumJsonConvert(), new JsonStringEnumConverter(), new ProviderMethodParametersJsonConvert() } }
-                ) is PingDepositResponse
+                ) is PingDepositResponseBody x &&
+                !string.IsNullOrWhiteSpace(x.ProviderMethodResponse.Reference)
             );
     }
 }
