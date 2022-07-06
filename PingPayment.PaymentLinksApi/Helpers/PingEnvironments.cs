bb46cfdbfe1d93560e126a724859048491ab63a7
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-07-06 13:43:57 +0200

    fixed productionUri

--- PingPayment.PaymentLinksApi/Helpers/PingEnvironments.cs
+++ PingPayment.PaymentLinksApi/Helpers/PingEnvironments.cs
@@ -3,6 +3,6 @@
     public static class PingEnvironments
     {
         public static string SandboxUri => "https://sandbox.pingpayments.com/payment_links/";
-        public static string ProductionUri => "https://pingpayments.com/payment_links/s";
+        public static string ProductionUri => "https://production.pingpayments.com/payment_links/";
     }
 }
