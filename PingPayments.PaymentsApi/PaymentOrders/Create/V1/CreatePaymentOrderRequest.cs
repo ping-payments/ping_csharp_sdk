74fb21f55f42e77b518ca6e271349d8f8e2ed33a
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-07-13 15:05:42 +0200

    Split Parameters Implemented for payment orders

--- PingPayments.PaymentsApi/PaymentOrders/Create/V1/CreatePaymentOrderRequest.cs
+++ PingPayments.PaymentsApi/PaymentOrders/Create/V1/CreatePaymentOrderRequest.cs
@@ -1,17 +1,24 @@
 ﻿using PingPayments.PaymentsApi.Payments.Shared.V1;
 using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace PingPayments.PaymentsApi.PaymentOrders.Create.V1
 {
 
-    public record CreatePaymentOrderRequest(CurrencyEnum Currency, Guid? SplitTreeId = null)
+    public record CreatePaymentOrderRequest(CurrencyEnum Currency, IDictionary<string, dynamic>? SplitParamters = null, Guid ? SplitTreeId = null) 
     {
         /// <summary>
         /// The currencu which will be used in the payment order
         /// </summary>
         [JsonPropertyName("currency")]
         public CurrencyEnum Currency { get; set; } = Currency;
+
+        /// <summary>
+        /// Parameters used to control parts of the split tree
+        /// </summary>
+        [JsonPropertyName("split_parameters")]
+        public IDictionary<string, dynamic>? SplitParamters { get; set; } = SplitParamters ?? new Dictionary<string, dynamic>();
 
         /// <summary>
         /// The split tree used for the payment order
