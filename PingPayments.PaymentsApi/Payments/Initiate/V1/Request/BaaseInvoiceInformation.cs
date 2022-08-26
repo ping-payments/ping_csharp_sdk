c11e653ef87acd7f203264c7c4068b8f7675b70e
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-08-26 14:07:50 +0200

    Pascalcase

--- PingPayments.PaymentsApi/Payments/Initiate/V1/Request/BaaseInvoiceInformation.cs
+++ PingPayments.PaymentsApi/Payments/Initiate/V1/Request/BaaseInvoiceInformation.cs
@@ -7,19 +7,19 @@ namespace PingPayments.PaymentsApi.Payments.V1.Initiate.Request
         string County,
         string FirstName,
         string LastName,
-        string postalCode,
-        string streetAddress,
-        string? invoiceLogoUrl = null
+        string PostalCode,
+        string StreetAddress,
+        string? InvoiceLogoUrl = null
     )
     {
         public Dictionary<string, dynamic> ToDictionary() => new()
         {
             { "county", County },
             { "first_name", FirstName },
-            { "invoice_logo_url", invoiceLogoUrl ?? string.Empty},
+            { "invoice_logo_url", InvoiceLogoUrl ?? string.Empty },
             { "last_name", LastName },
-            { "postal_code", postalCode },
-            { "street_address", streetAddress },
+            { "postal_code", PostalCode },
+            { "street_address", StreetAddress },
         };
     }
 }
