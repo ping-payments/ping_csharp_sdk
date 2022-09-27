b6e48fe728df0e8b74b8e3288b6cce2194fffffb
LJsson <ludvig.johansson@live.se>
2022-09-27 10:05:43 +0200

    payment_provider_methods cointains expected items

--- PingPayments.PaymentsApi/Tenants/Shared/PaymentProviderMethod.cs
+++ PingPayments.PaymentsApi/Tenants/Shared/PaymentProviderMethod.cs
@@ -1,6 +1,14 @@
-﻿namespace PingPayments.PaymentsApi.Tenants.Shared
+﻿using PingPayments.Shared.Enums;
+using System.Text.Json.Serialization;
+
+namespace PingPayments.PaymentsApi.Tenants.Shared
 {
     public record PaymentProviderMethod
     {
+        [JsonPropertyName("method")]
+        public MethodEnum Method { get; set; }
+
+        [JsonPropertyName("provider")]
+        public ProviderEnum Provider { get; set; }
     }
 }
