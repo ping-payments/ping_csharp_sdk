0bb531a626969c56da44efed6184bc10d8f8a0e4
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-09-27 17:51:37 +0200

    Added test with correct name

--- PingPayments.Mimic.Tests/V1/DepositTests.cs
+++ PingPayments.Mimic.Tests/V1/DepositTests.cs
@@ -7,18 +7,11 @@ namespace PingPayments.Mimic.Tests.V1
     {
 
         [Fact]
-        public async Task Update_merchant_status_returns_204()
+        public async Task Create_deposit_returns_204()
         {
             var request = new CreateDepositRequest(2000, CurrencyEnum.SEK, "OCR", "100817790");
             var response = await _api.Deposit.V1.Create(request);
             AssertHttpNoContent(response);
-        }
-
-        [Fact]
-        public async Task Update_merchant_status_returns_422()
-        {
-            var response = await _api.Merchant.V1.Update(Guid.NewGuid(), MerchantStatus.APPROVED);
-            AssertHttpUnprocessableEntity(response);
         }
     }
 }
