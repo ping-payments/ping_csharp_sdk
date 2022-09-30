3e3a3ee15ea7651ea29d3284dd2832d8edac9ee6
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-09-30 10:21:36 +0200

    added refrence to Settlements

--- PingPayments.PaymentsApi/Disbursements/Shared/Settlements.cs
+++ PingPayments.PaymentsApi/Disbursements/Shared/Settlements.cs
@@ -8,20 +8,30 @@ namespace PingPayments.PaymentsApi.Disbursements.Shared
         [JsonPropertyName("amount")]
         public int Amount { get; set; }
 
+
         [JsonPropertyName("label")]
         public string? Label { get; set; }
+
 
         [JsonPropertyName("merchant_id")]
         public Guid MerchantId { get; set; }
 
+
+        [JsonPropertyName("reference")]
+        public string? Reference { get; set; }
+
+
         [JsonPropertyName("payment_id")]
         public Guid? PaymentId { get; set; }
+
 
         [JsonPropertyName("payment_order_id")]
         public Guid PaymentOrderId { get; set; }
 
+
         [JsonPropertyName("recipient_name")]
         public string RecipientName { get; set; }
+
 
         [JsonPropertyName("recipient_type")]
         public RecipientTypeEnum RecipientType { get; set; }
