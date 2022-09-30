89ca2c5c6e52cf28bf0c41b9adab3e699c2f588e
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-09-30 10:58:50 +0200

    Added summary

--- PingPayments.PaymentsApi/Disbursements/Shared/Settlements.cs
+++ PingPayments.PaymentsApi/Disbursements/Shared/Settlements.cs
@@ -5,34 +5,60 @@ namespace PingPayments.PaymentsApi.Disbursements.Shared
 {
     public record Settlements
     {
+        /// <summary>
+        /// The settled amount in minor currency
+        /// </summary>
         [JsonPropertyName("amount")]
         public int Amount { get; set; }
 
 
+        /// <summary>
+        /// A lable describing the settlement
+        /// </summary>
         [JsonPropertyName("label")]
         public string? Label { get; set; }
 
 
+        /// <summary>
+        /// The Merchant which will receive the settled amount. This value will be null if the type isn't Merchant,
+        /// which means the settled amount wasn't paid out to a Merchant but the Tenant or payment facilitator
+        /// </summary>
         [JsonPropertyName("merchant_id")]
         public Guid MerchantId { get; set; }
 
 
+        /// <summary>
+        /// The reference attached to the payout
+        /// </summary>
         [JsonPropertyName("reference")]
         public string? Reference { get; set; }
 
 
+        /// <summary>
+        /// Which Payment the settled amount can be derived from.
+        /// This value will be null if the amount can not be derived from a specific payment
+        /// </summary>
         [JsonPropertyName("payment_id")]
         public Guid? PaymentId { get; set; }
 
 
+        /// <summary>
+        /// Which PaymentOrder the settled amount can be derived from.
+        /// </summary>
         [JsonPropertyName("payment_order_id")]
         public Guid PaymentOrderId { get; set; }
 
 
+        /// <summary>
+        /// The name of the party which the settled amount was paid out to
+        /// </summary>
         [JsonPropertyName("recipient_name")]
         public string RecipientName { get; set; }
 
 
+        /// <summary>
+        /// An enum describing which entity the settled amount was paid out to.
+        /// </summary>
         [JsonPropertyName("recipient_type")]
         public RecipientTypeEnum RecipientType { get; set; }
     }
