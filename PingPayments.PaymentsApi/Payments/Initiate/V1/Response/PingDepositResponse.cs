0cfef76bf1742766d0d4f55750a3b7af66a89412
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-10-12 16:00:35 +0200

    added DepositAccount to PingDepositResponse

--- PingPayments.PaymentsApi/Payments/Initiate/V1/Response/PingDepositResponse.cs
+++ PingPayments.PaymentsApi/Payments/Initiate/V1/Response/PingDepositResponse.cs
@@ -1,10 +1,14 @@
-﻿using System.Text.Json.Serialization;
+﻿using PingPayments.PaymentsApi.Payments.Shared.V1;
+using System.Text.Json.Serialization;
 
 namespace PingPayments.PaymentsApi.Payments.V1.Initiate.Response
 {
     public record PingDepositResponse
     {
         [JsonPropertyName("reference")]
-        public string Reference { get; set; }        
+        public string Reference { get; set; }
+
+        [JsonPropertyName("deposit_account")]
+        public DepositAccount DepositAccount { get; set; }
     }
 }
