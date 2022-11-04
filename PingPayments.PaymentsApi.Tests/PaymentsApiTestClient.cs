ce295e088e5d9bcbc4124b3f4733b9ccf87ff95c
LJsson <ludvig.johansson@live.se>
2022-11-04 10:52:24 +0100

    added mimic client

--- PingPayments.PaymentsApi.Tests/PaymentsApiTestClient.cs
+++ PingPayments.PaymentsApi.Tests/PaymentsApiTestClient.cs
@@ -1,4 +1,5 @@
-﻿using PingPayments.PaymentsApi.Helpers;
+﻿using PingPayments.Mimic;
+using PingPayments.PaymentsApi.Helpers;
 using PingPayments.Shared;
 using PingPayments.Tests;
 using System.Net.Http;
@@ -8,12 +9,16 @@ namespace PingPayments.PaymentsApi.Tests
     public class PaymentsApiTestClient : BaseResourceTests
     {
         protected readonly IPingPaymentsApiClient _api;
+        protected readonly IPingMimicApiClient _mimicApi;
         private readonly HttpClient _httpClient;
+        private readonly HttpClient _mimicHttpClient;
 
         public PaymentsApiTestClient()
         {
             _httpClient = new HttpClient().ConfigurePingPaymentsClient(PingEnvironments.PaymentsApi.SandboxUri, TestData.TenantId);
+            _mimicHttpClient = new HttpClient().ConfigurePingPaymentsClient(Mimic.Helpers.PingEnvironments.MimicApi.SandboxUri, TestData.TenantId);
             _api = new PingPaymentsApiClient(_httpClient);
+            _mimicApi = new PingMimicApiClient(_mimicHttpClient);
         }
     }
 }
