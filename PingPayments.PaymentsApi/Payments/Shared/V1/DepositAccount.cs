a6ab519f266573e79bec3071c6565138f449ee36
Gustav Fransson <gustavfransson.extra@gmail.com>
2023-02-21 13:12:09 +0100

    added bban and bic to deposti account

--- PingPayments.PaymentsApi/Payments/Shared/V1/DepositAccount.cs
+++ PingPayments.PaymentsApi/Payments/Shared/V1/DepositAccount.cs
@@ -7,6 +7,12 @@ namespace PingPayments.PaymentsApi.Payments.Shared.V1
         [JsonPropertyName("iban")]
         public string Iban { get; set; }
 
+        [JsonPropertyName("bban")]
+        public string bban { get; set; }
+
+        [JsonPropertyName("bic")]
+        public string bic { get; set; }
+
         [JsonPropertyName("bankgiro_number")]
         public string BankgiroNumber { get; set; }
     }
