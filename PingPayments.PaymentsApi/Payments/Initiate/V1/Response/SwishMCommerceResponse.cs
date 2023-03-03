b642f51f1e7c183ef3d7bf577a5fc4ec1ddd9706
LJsson <ludvig.johansson@live.se>
2023-03-03 10:06:45 +0100

    Request token added to swish response

--- PingPayments.PaymentsApi/Payments/Initiate/V1/Response/SwishMCommerceResponse.cs
+++ PingPayments.PaymentsApi/Payments/Initiate/V1/Response/SwishMCommerceResponse.cs
@@ -12,9 +12,15 @@ namespace PingPayments.PaymentsApi.Payments.V1.Initiate.Response
         public string SwishUrl { get; set; }
 
         /// <summary>
+        /// Used for opening Swish in Mobile Phone by redirect. Does not contain callbackUrl.
+        /// </summary>
+        [JsonPropertyName("payment_request_token")]
+        public string PaymentRequestToken { get; set; }
+
+        /// <summary>
         /// Used for displaying Qr Code for scanning of swish payment
         /// </summary>
         [JsonPropertyName("qr_code")]
         public string? QrCode { get; set; }
-    }    
+    }
 }
