c1c904797863f93ccd4db2fc34527d960915ffb5
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2023-06-01 14:14:07 +0200

    Adding get templates test

---
+++ PingPayments.KYC.Tests/V1/AgreementTests.cs
@@ -0,0 +1,20 @@
+﻿using PingPayments.KYC.Merchant.V1.Get;
+using PingPayments.Tests;
+
+namespace PingPayments.KYC.Tests.V1
+{
+    public class AgreementTests : KYCApiTestClient
+    {
+        [Fact]
+        public async Task Get_templates_returns_200()
+        {
+            var request = new GetKycRequest
+            {
+                MerchantId = TestData.MerchantId
+            };
+            var response = await _api.Agreement.V1.GetAgreementTemplates();
+            AssertHttpOK(response);
+            Assert.True(response.Body.SuccessfulResponseBody.Any());
+        }
+    }
+}
