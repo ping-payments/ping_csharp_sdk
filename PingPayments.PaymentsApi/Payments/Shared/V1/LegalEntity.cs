0262d2366c01759e224d3acd4b3cee994593218d
LJsson <ludvig.johansson@live.se>
2024-01-10 15:10:54 +0100

    nullable legal entity

--- PingPayments.PaymentsApi/Payments/Shared/V1/LegalEntity.cs
+++ PingPayments.PaymentsApi/Payments/Shared/V1/LegalEntity.cs
@@ -7,22 +7,23 @@ namespace PingPayments.PaymentsApi.Payments.Shared.V1
 {
     public record LegalEntity
     {
+
         /// <summary>
         /// Two letter ISO 3166-2 country code
         /// </summary>
         [JsonPropertyName("country")]
-        public string Country { get; set; }
+        public string? Country { get; set; }
 
         /// <summary>
         /// Unique identifier, e.g. personal identification number, organization number
         /// </summary>
         [JsonPropertyName("identifier")]
-        public string Identifier { get; set; }
+        public string? Identifier { get; set; }
 
         /// <summary>
         /// Type denoting whether the legal entity is an organization or a person
         /// </summary>
         [JsonPropertyName("type")]
-        public LegalEntityEnum Type { get; set; }
+        public LegalEntityEnum? Type { get; set; }
     }
 }
