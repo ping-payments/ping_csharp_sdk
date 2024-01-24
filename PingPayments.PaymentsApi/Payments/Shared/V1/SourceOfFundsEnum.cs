fa81e93dedf7a870f5cde78a4d2d9a02966352c7
LJsson <ludvig.johansson@live.se>
2024-01-24 13:55:47 +0100

    added Enum

---
+++ PingPayments.PaymentsApi/Payments/Shared/V1/SourceOfFundsEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace PingPayments.PaymentsApi.Payments.Shared.V1
+{
+    public enum SourceOfFundsEnum
+    {
+        salary_or_employment_income,
+        business_profits,
+        inheritance,
+        sale_of_assets,
+        dividends_and_investment_income,
+        loan_or_credit,
+        gifts_and_donations,
+        insurance_payouts,
+        retirement_funds,
+        legal_settlements,
+        rental_income,
+        royalties,
+        lottery_or_gambling_winnings,
+        foreign_remittances
+    }
+}
