51ad35178060759ccd840029d4b12fdf493dd8fb
LJsson <luddis6904@gmail.com>
2024-03-04 16:50:39 +0100

    add response

--- PingPayments.PaymentsApi/Payments/Initiate/V1/Response/PingDepositResponse.cs
+++ PingPayments.PaymentsApi/Payments/Initiate/V1/Response/PingDepositResponse.cs
@@ -1,4 +1,5 @@
 ﻿using PingPayments.PaymentsApi.Payments.Shared.V1;
+using PingPayments.PaymentsApi.Payments.Shared.V1.Deposit;
 using System;
 using System.Text.Json.Serialization;
 
@@ -11,6 +12,9 @@ namespace PingPayments.PaymentsApi.Payments.V1.Initiate.Response
 
         [JsonPropertyName("bank_account")]
         public BankAccount BankAccount { get; set; }
+
+        [JsonPropertyName("invoice")]
+        public InvoiceUrls Invoice { get; set; }
 
         [JsonPropertyName("deposit_account")]
         [Obsolete("Replaced with BankAccount.")]
