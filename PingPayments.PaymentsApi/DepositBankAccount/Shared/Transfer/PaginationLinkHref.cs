7c3f1728c93384690ce4d068e9f04160ddf41c34
LJsson <luddis6904@gmail.com>
2024-03-28 14:32:55 +0100

    removed nullable

--- PingPayments.PaymentsApi/DepositBankAccount/Shared/Transfer/PaginationLinkHref.cs
+++ PingPayments.PaymentsApi/DepositBankAccount/Shared/Transfer/PaginationLinkHref.cs
@@ -5,7 +5,7 @@ namespace PingPayments.PaymentsApi.DepositBankAccount.Shared.Transfer
     public record PaginationLinkHref
     {
         [JsonPropertyName("href")]
-        public string? Href { get; set; }
+        public string Href { get; set; }
     }
 
 }
