87bef2ec4255b20408cc7611bb14a7644c56229a
Magnus Sjögren <magnus@pennybridge.org>
2025-10-03 10:18:28 +0200

    Rename class to align with Ping naming convention

--- PingPayments.PaymentsApi/Payments/Initiate/V1/Request/CreatePayment.VippsMobilepay.cs
+++ PingPayments.PaymentsApi/Payments/Initiate/V1/Request/CreatePayment.VippsMobilepay.cs
@@ -7,7 +7,7 @@ namespace PingPayments.PaymentsApi.Payments.V1.Initiate.Request
 {
     public static partial class CreatePayment
     {
-        public static class Vipps
+        public static class VippsMobilepay
         {
             public static InitiatePaymentRequest MobilePay
             (
